12c4f54a0df79765b77f0429b791b51b94cebba5
Qexat <43090614+qexat@users.noreply.github.com>
2025-10-22 08:30:52 +0200

    add contravariant functor class

--- lib/Class/Functor.ml
+++ lib/Class/Functor.ml
@@ -7,3 +7,17 @@ module Weak (P : Type.POLY) = struct
 end
 
 include Weak (Type.Id)
+
+module Contravariant = struct
+  module Weak (P : Type.POLY) = struct
+    module type TYPE = sig
+      include Type.POLY
+
+      val map
+        : 'a 'b.
+        ('a P.t -> 'b P.t) -> 'b P.t t -> 'a P.t t
+    end
+  end
+
+  include Weak (Type.Id)
+end
