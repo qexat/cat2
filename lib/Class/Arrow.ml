a7a8bb66468e530d2f5ca4a4488b1faf67e301d3
Qexat <43090614+qexat@users.noreply.github.com>
2025-10-22 09:07:25 +0200

    add arrow class

---
+++ lib/Class/Arrow.ml
@@ -0,0 +1,21 @@
+open Base
+
+module Weak (P : Type.POLY) = struct
+  open Category.Weak (P)
+
+  module type TYPE = sig
+    include TYPE
+
+    val arrow : 'a 'b. ('a P.t -> 'b P.t) -> ('a P.t, 'b P.t) t
+
+    val ( *** )
+      : 'a1 'a2 'b1 'b2.
+      ('a1 P.t, 'b1 P.t) t
+      -> ('a2 P.t, 'b2 P.t) t
+      -> ( ('a1 P.t, 'a2 P.t) Product.t
+           , ('b1 P.t, 'b2 P.t) Product.t )
+           t
+  end
+end
+
+include Weak (Type.Id)
